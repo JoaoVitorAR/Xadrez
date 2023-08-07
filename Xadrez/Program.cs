0b255c8e35babf9d698f34125f0a188ccd41e167
JoaoVitorAR <jvalves1998@outlook.com.br>
2023-08-07 10:15:36 -0300

    Inciando peças no tabuleiro

--- Xadrez/Program.cs
+++ Xadrez/Program.cs
@@ -1,4 +1,5 @@
 ﻿using tabuleiro;
+using xadrez;
 
 namespace Xadrez
 {
@@ -6,7 +7,12 @@ namespace Xadrez
     {
         static void Main(string[] args)
         {
-           Tabuleiro tab = new Tabuleiro(8,8);
+
+            Tabuleiro tab = new Tabuleiro(8,8);
+
+            tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+            tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
+            tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(2, 4));
 
             Tela.imprimirTabuleiro(tab);
         }
